8334b3aae9c48a34104bcb8459dd84eb62446727
Dustin <swed04201@gmail.com>
2022-01-26 19:10:04 +0900

    add pv instance data generator -> consider time to generate data

--- generator.py
+++ generator.py
@@ -5,9 +5,10 @@ Created on Jan 8, 2022
 @author: Dustin Lin
 '''
 import random
-import time
+import time, datetime
 from tool import result, write_log_file, config_set, config_get, send_request, verify_init_value_isexist
-
+start_time = 4
+end_time = 20
 class generator():
     def __init__(self):
         pass
@@ -15,13 +16,21 @@ class generator():
     def pv_instance(self, device_type, device_no, sleep_time):
         str_url = config_get("api_info", "pv_url").format(device_no, config_get("device_epc", "pv_instance"))
         while(True):
-            str_value = str(random.randrange(0, 1000))
-            str_data = str_value.zfill(4)
-            send_request(str_url, str_data)
-            print("Sleep for {} secs!!!".format(sleep_time))
-            
+            int_now_hour = int(datetime.datetime.now().strftime('%H'))
+            if int_now_hour >= start_time and int_now_hour < end_time:
+                print("into if")
+                str_value = str(random.randrange(0, 1000))
+                str_data = str_value.zfill(4)
+                send_request(str_url, str_data)
+                print("Sleep for {} secs!!!".format(sleep_time))
+            elif int_now_hour < start_time or int_now_hour >= end_time:
+                print("into elif")
+                str_value = str(0)
+                str_data = str_value.zfill(4)
+                send_request(str_url, str_data)
+                print("Sleep for {} secs!!!".format(sleep_time))    
             time.sleep(int(sleep_time))
-
+            
     def battery_instance(self, device_type, device_no, sleep_time):
         str_url = config_get("api_info", "battery_url").format(device_no, config_get("device_epc", "battery_elec_3"))
         while(True):
