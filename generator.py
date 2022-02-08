26f5798f42b84ddd2b9be62e3a803c716c5d8af9
Dustin <swed04201@gmail.com>
2022-02-08 16:59:31 +0900

    update accumulate func

--- generator.py
+++ generator.py
@@ -43,28 +43,6 @@ class generator():
             send_request(str_url, str_data)
             print("Sleep for {} secs!!!".format(sleep_time))
             time.sleep(int(sleep_time))    
-
-    
-    """def accumulate(self, device_type, device_no, scope, sleep_time):
-        epc = config_get("device_epc", scope)
-        scope = device_no + "_" + config_get("device_epc", scope)
-        verify_init_value_isexist(device_type, scope)
-        config_value = device_type + "_url"
-        str_url = config_get("api_info", config_value).format(device_no, epc)
-        str_device_scope = device_no + "_" + epc
-        int_start_data = int(config_get(device_type, str_device_scope))
-        int_data = int_start_data
-        while(True):
-            str_data = str(int_data)
-            str_format_data = str_data.zfill(8)
-            int_data = int_data + random.randrange(400, 2000, 400)
-            send_request(str_url, str_format_data)
-            msg = "Device: {} EPC: {} --> ".format(device_type, epc)
-            msg = msg + result(int_data, int_start_data)
-            write_log_file(msg)
-            config_set(device_type, str_device_scope, str(int_data))         
-            print("Sleep for {} secs!!!".format(sleep_time))
-            time.sleep(int(sleep_time))"""
     
     def accumulate(self, device_type, device_no, scope, sleep_time):
         epc = config_get("device_epc", scope)
@@ -81,24 +59,17 @@ class generator():
                 str_data = str(int_data)
                 str_format_data = str_data.zfill(8)
                 int_data = int_data + random.randrange(400, 2000, 400)
-                send_request(str_url, str_format_data)
-                msg = "Device: {} EPC: {} --> ".format(device_type, epc)
-                msg = msg + result(int_data, int_start_data)
-                write_log_file(msg)
-                config_set(device_type, str_device_scope, str(int_data))         
-                print("Sleep for {} secs!!!".format(sleep_time))
-                time.sleep(int(sleep_time))
             elif int_now_hour < start_time or int_now_hour >= end_time:
                 str_data = str(config_get(device_type, str_device_scope))
                 str_format_data = str_data.zfill(8)
-                send_request(str_url, str_format_data)
-                msg = "Device: {} EPC: {} --> ".format(device_type, epc)
-                msg = msg + result(int_data, int_start_data)
-                write_log_file(msg)
-                config_set(device_type, str_device_scope, str(int_data))         
-                print("Sleep for {} secs!!!".format(sleep_time))
-                time.sleep(int(sleep_time))
-    
+            send_request(str_url, str_format_data)
+            msg = "Device: {} EPC: {} --> ".format(device_type, epc)
+            msg = msg + result(int_data, int_start_data)
+            write_log_file(msg)
+            config_set(device_type, str_device_scope, str(int_data))         
+            print("Sleep for {} secs!!!".format(sleep_time))
+            time.sleep(int(sleep_time))
+        
     
         
 if __name__ == '__main__':
